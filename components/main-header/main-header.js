0f2d8234c7ad0b6e3299b071abdef8676516545f
jinseonghee <wlstjdgml0610@gmail.com>
2024-02-03 16:47:42 +0900

    feat: main page 내용 채우기 (~102)

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,11 +2,15 @@ import Link from "next/link";
 import logoImg from '@/assets/logo.png';
 import Image from "next/image";
 import classes from './main-header.module.css';
+import { MainHeaderBackground } from "./main-header-background";
 
 export default function MainHeader() {
-    return <header className={classes.header}>
+    return (
+        <>
+       <MainHeaderBackground/>
+        <header className={classes.header}>
         <Link className={classes.logo} href="/">
-            <Image src={logoImg} alt="A plate with food on it" priority/>
+            <Image src={logoImg} alt="A plate with food on it" priority/>{/*header img는 항상 제일먼저 로딩되어하기 때무넹 priority */}
             NextLevel Food
         </Link>
         <nav className={classes.nav}>
@@ -20,4 +24,6 @@ export default function MainHeader() {
             </ul>
         </nav>
     </header>
+    </>
+    )
 }
